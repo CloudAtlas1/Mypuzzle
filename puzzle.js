ca089c43228d960be492cc924794ae3ccd217aa9
Cloud <CloudAtlas@yeah.net>
2023-04-28 01:23:13 +0800

    puzzle

---
+++ puzzle.js
@@ -0,0 +1,18 @@
+function Puzzle1(){
+  var response = prompt("Please choose a prime number from 8,17,15,21,and write it down.");
+  if (response === '17') {
+  alert('That is right,congratulations！');
+} else {
+  alert('Sorry, your answer is wrong,please try again.');
+}}
+
+function Puzzle2(){
+  var response = prompt("Why is a crow like a writing desk?");
+  if (response === 'Because they can all produce a few notes') {
+  alert('You are right,Congratulations!');
+}
+  else if (response === 'Because I like you') {
+  alert('Congratulations on finding the hidden answer! I like you too！');
+} else {
+  alert('Well,please try again.');
+}}
